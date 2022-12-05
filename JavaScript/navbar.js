fa3a9f344ca2731b890b2dd525b95234ee176581
Eloy <eloybermejo@inspladelestany.cat>
2022-12-05 15:03:33 +0100

    Canvis petits

--- JavaScript/navbar.js
+++ JavaScript/navbar.js
@@ -1,10 +1,9 @@
 var navBar = document.getElementById("navBar");
 navBar.innerHTML = `
-<nav class="navbar fixed-top navbar-expand-sm navbar-light py-2">
+<nav class="navbar fixed-top navbar-expand navbar-light py-2">
     <div class="container-fluid">
       <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarExample01"
         aria-controls="navbarExample01" aria-expanded="false" aria-label="Toggle navigation">
-        <i class="fas fa-bars"></i>
       </button>
       <div class="collapse navbar-collapse" id="navbarExample01">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
@@ -12,13 +11,13 @@ navBar.innerHTML = `
             <a class="nav-link" href="index.html"><img src="./icons/home.svg" width="30" height="24"><img></a>
           </li>
           <li id="pag1" class="nav-item">
-            <a class="nav-link" href="pag1.html" target="_blank">Tipus isomètric</a>
+            <a class="nav-link" href="pag1.html">Tipus isomètric</a>
           </li>
           <li id="pag2" class="nav-item">
-            <a class="nav-link" href="pag2.html" target="_blank">Tipus Top-Bottom</a>
+            <a class="nav-link" href="pag2.html">Tipus Top-Bottom</a>
           </li>
           <li id="pag3" class="nav-item">
-            <a class="nav-link" href="pag3.html" target="_blank">Tipus 2D</a>
+            <a class="nav-link" href="pag3.html">Tipus 2D</a>
           </li>
         </ul>
       </div>
